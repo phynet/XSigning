1e166105d91675d35ebf5e8bef1fdf01c5e1f318
phynet <sofia.swidarowicz@gmail.com>
2015-03-02 21:36:14 +0100

    Changed CL options. Added list cert & provisioning from CL too.

--- main.rb
+++ main.rb
@@ -8,26 +8,54 @@ class Main < SetValues
 
   set_values = SetValues.new
 
-  puts 'Write Provisioning Profile name:'
-  @provisioning_profile_name = gets.chomp
 
-  puts 'Write Certificate name:'
-  @certificate_profile_name = gets.chomp
+  if ARGV.count > 3
+    @certificate_profile_name = ARGV[0].dup
+    puts "Using Certificate name: #{@certificate_profile_name}"
 
-  puts 'Write path xcodeproj:'
-  @path_xcode = gets.chomp
+    @provisioning_profile_name = ARGV[1].dup
+    puts "Using Provisioning Profile name: #{@provisioning_profile_name}" 
 
-  puts 'Searching provisioning....'
+    @path_xcode = ARGV[2].dup
+    puts "Using path xcodeproj: #{@path_xcode}"
 
-  set_values.path_provisioning
+    @provisioning_profile_path = ARGV[3].dup
+    puts "Using Provisioning Profile path: #{@provisioning_profile_path}"
 
-  array_provisioning = set_values.list_all_provisioning
-  array_uuid, array_name_prov = set_values.query_security_keychain(array_provisioning)
+    puts 'Searching provisioning and certificates....'
 
-  provisioning_uuid = set_values.validate_provisioning(array_uuid,array_name_prov,@provisioning_profile_name)
+    set_values.path_provisioning(@provisioning_profile_path)
 
-  set_values.validate_codesign_identity(@certificate_profile_name)
-  set_values.validate_path_xcode(@path_xcode)
-  set_values.write_in_XCProject(provisioning_uuid,@certificate_profile_name)
+    array_provisioning = set_values.list_of_all_provisioning
+    array_uuid, array_name_prov = set_values.query_security_keychain(array_provisioning)
 
+    provisioning_uuid = set_values.validate_provisioning(array_uuid,array_name_prov,@provisioning_profile_name)
+
+    set_values.validate_codesign_identity(@certificate_profile_name)
+    set_values.validate_path_xcode(@path_xcode)
+    set_values.write_in_XCProject(provisioning_uuid,@certificate_profile_name)
+
+  else
+     ARGV.each do |opt|
+        if opt.eql? 'cert'
+          list_certs = set_values.search_all_certificates
+          format_array = Utils::formatting_string_certificate(list_certs)
+          puts format_array
+        elsif opt.eql? 'prov'
+          @provisioning_profile_path = ARGV[2].dup
+          puts "Using Provisioning Profile path: #{@provisioning_profile_path}"
+          set_values.path_provisioning(@provisioning_profile_path)
+          array_provisioning = set_values.list_of_all_provisioning
+          array_name_prov = set_values.query_security_keychain(array_provisioning)
+          puts set_values.list_all_provisioning_profiles(array_name_prov)
+        end
+      end
+  end
+  
+ 
+
+
+  
+
+  
 end
--- set_values_calabash.rb
+++ set_values_calabash.rb
@@ -1,4 +1,4 @@
-class SetValues
+class SetValuesCalabash
   require 'fileutils'
   include FileUtils
   require_relative 'Utils'
@@ -7,22 +7,18 @@ class SetValues
     FileUtils.cd(path, :verbose => true)
   end
 
-  def path_provisioning(path)
-    puts "enters to path provisioning #{path}"
-    abort "Provisioning profile path #{path} does not exist. Search for it in terminal." unless File.exists? path
-    move_to_path(path)
-  end
-
   def validate_path_xcode(path_xcode)
     @path_xcode = path_xcode
     abort "Xcodeproj path '#{path_xcode}' does not exist" unless File.exists? path_xcode
-    FileUtils.cd(path_xcode, :verbose => true)
   end
 
-  def list_of_all_provisioning
-    return Dir.glob("*mobileprovision*")
+  def list_all_provisioning path
+    puts 'enters to provisioning path'
+    abort "Provisioning profile path '#{path}' does not exist. Tell this to the developer." unless File.exists? path
+    return Dir.glob("#{path}/*mobileprovision*")
   end
 
+  ## a loop through all mobileprovisioning and save it in a file .xml
   def query_security_keychain(array)
    puts 'enters security keychain...'
 
@@ -30,7 +26,7 @@ class SetValues
     name_certs = []
 
     array.each do |actual_provisioning|
-      command="security cms -D -i #{actual_provisioning}"
+      command="security cms -D -i \"#{actual_provisioning}\""
       value_provisioning=%x(#{command})
 
       value_provisioning=~/<key>UUID<\/key>.*?<string>(.*?)<\/string>/im
@@ -42,18 +38,28 @@ class SetValues
     return uuid, name_certs
   end
 
+
   def write_in_XCProject(uuid_prov, user_cert_name)
-    @path_xcode << '/' << Dir.glob("*xcodeproj*")[0] << '/project.pbxproj'
+    @path_xcode = Dir.glob("#{@path_xcode}/*xcodeproj*")[0] << '/project.pbxproj'
+    puts @path_xcode
 
-    prov = "PROVISIONING_PROFILE = \"#{uuid_prov}\";"
-    cert = "CODE_SIGN_IDENTITY = \"#{user_cert_name}\";" 
-    cert_sdk = /\"CODE_SIGN_IDENTITY\[sdk=iphoneos\*\]\"\s=\s"(.*?)";$/
-    cert_sdk_sub = "\"CODE_SIGN_IDENTITY[sdk=iphoneos*]\" = \"#{user_cert_name}\";"
+    #prov = "PROVISIONING_PROFILE = \"#{uuid_prov}\";"
+    #cert = "CODE_SIGN_IDENTITY = \"#{user_cert_name}\";"
+     
+     prov =/<key>PROVISIONING_PROFILE<\/key>\s.*?<string>(.*?)<\/string>/mi
+     cert =/<key>CODE_SIGN_IDENTITY<\/key>\s.*?<string>(.*?)<\/string>/mi
 
-    File.open(filename = @path_xcode, "r+") { |file| file << File.read(filename).gsub(/PROVISIONING_PROFILE\s=\s"(.*?)";$/,"#{prov}") }
-    File.open(filename = @path_xcode, "r+") { |file| file << File.read(filename).gsub(/CODE_SIGN_IDENTITY\s=\s"(.*?)";$/,"#{cert}") }
-    File.open(filename = @path_xcode, "r+") { |file| file << File.read(filename).gsub(cert_sdk,cert_sdk_sub) }
-    
+     prov1="<key>PROVISIONING_PROFILE</key>" + "\n"
+     prov2= "                <string>#{uuid_prov}</string>"
+     prov3 = prov1+prov2
+
+     cert1="<key>CODE_SIGN_IDENTITY</key>"+"\n"
+     cert2= "                <string>#{user_cert_name}</string>"
+     cert3 = cert1+cert2
+
+     File.open(filename = @path_xcode , "r+") { |file| file << File.read(filename).gsub(prov,"#{prov3}") }
+     File.open(filename = @path_xcode , "r+") { |file| file << File.read(filename).gsub(cert,"#{cert3}") }
+
     puts 'File changed...'
     puts 'Done with signing...'
   end
@@ -70,15 +76,18 @@ class SetValues
 
   def check_for_match_certificate(certificate_name_array, user_certificate_name)
     format_array = Utils::formatting_string_certificate(certificate_name_array)
-    return false, format_array unless format_array.include? user_certificate_name
+     unless format_array.include? user_certificate_name
+      return false, format_array
     else
       return true
+    end
   end
 
   def validate_codesign_identity(user_certificate)
-    list_all_certs =  search_all_certificates
-    val_cert, list_format_cert =  check_for_match_certificate(list_all_certs,user_certificate)
+    command="/usr/bin/security find-identity -v -p codesigning"
+    list_all_certs = %x(#{command})
 
+    val_cert, list_format_cert = check_for_match_certificate(list_all_certs,user_certificate)
 
     unless val_cert
       puts ("Certificate Profile #{user_certificate} does not exist")
@@ -90,7 +99,7 @@ class SetValues
   def validate_provisioning(array_uuid,provisioning_profile_names,user_provisioning_profile_name)
 
     val_prov, uuid_prov = check_for_match_provisioning(array_uuid,provisioning_profile_names,user_provisioning_profile_name)
-
+   
     unless val_prov
         puts ("Provisioning Profile #{user_provisioning_profile_name} does not exist")
         list_all_provisioning_profiles(provisioning_profile_names)
@@ -108,15 +117,5 @@ class SetValues
     puts 'These are all code sign identity found'
     puts certificate_list
   end
-
-
-  def search_all_certificates
-    command="/usr/bin/security find-identity -v -p codesigning"
-    list_all_certs = %x(#{command})
-    return list_all_certs
-  end
-
-
-
 
 end
